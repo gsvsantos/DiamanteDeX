ddbb5c1a946c5102c2de617b981ef1cea4c8b36d
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-25 20:12:44 -0300

    feat: Adicionado `ViewUtils` no `IntVerify` para melhor interação com o usuário.

--- DiamanteDeX/Entities/Utils/Validators.cs
+++ DiamanteDeX/Entities/Utils/Validators.cs
@@ -6,33 +6,33 @@ using System.Threading.Tasks;
 
 namespace DiamanteDeX.Entities.Utils
 {
-    internal class Validators
+    public class Validators
     {
         public static int IntVerify(string prompt, int minValue = 0, int maxValue = int.MaxValue)
         {
             do
             {
-                Console.Write(prompt);
+                ViewUtils.PaintWrite(prompt);
                 string input = Console.ReadLine()!;
                 if (string.IsNullOrEmpty(input))
                 {
-                    Console.WriteLine($"'{input}' não é um número...");
+                    ViewUtils.PaintWriteLine($"'{input}' não é um número...", ConsoleColor.Red);
                     continue;
                 }
                 input = input.Trim();
                 if (input.Contains(' ') || input.Contains('.') || input.Contains(','))
                 {
-                    Console.WriteLine("O valor não pode conter espaço ' ', ponto (.), ou vírgula (,)!");
+                    ViewUtils.PaintWriteLine("O valor não pode conter espaço ' ', ponto (.), ou vírgula (,)!", ConsoleColor.Red);
                     continue;
                 }
                 if (!int.TryParse(input, out int value))
                 {
-                    Console.WriteLine("O valor digitado não é um número válido.");
+                    ViewUtils.PaintWriteLine("O valor digitado não é um número válido.", ConsoleColor.Red);
                     continue;
                 }
                 if (value <= minValue || value > maxValue)
                 {
-                    Console.WriteLine($"O valor deve estar entre 1 e {maxValue}.");
+                    ViewUtils.PaintWriteLine($"O valor deve estar entre 1 e {maxValue}.", ConsoleColor.Red);
                     continue;
                 }
                 return value;
