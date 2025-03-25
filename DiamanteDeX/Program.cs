f29bf6b6d44d129840dae55032a762b9c117898f
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-25 17:53:21 -0300

    feat: Criada funções para validação de par ou ímpar.

--- DiamanteDeX/Program.cs
+++ DiamanteDeX/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace DiamanteDeX
+﻿using System.Text.Json.Serialization.Metadata;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DiamanteDeX
 {
     internal class Program
     {
+        public static int number;
         static void Main(string[] args)
         {
-            Console.WriteLine("Projeto - Diamante de X");
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Projeto - Diamante de X\n");
+                number = GetNumber("\nPor favor, digite um número inteiro para dar início ao desenho do diamante: ");
+                OddOrEven();
+                Console.WriteLine($"\nO número ímpar escolhido é: {number}");
+                Console.ReadKey();
+            }while (true);
+        }
+        static int GetNumber(string prompt)
+        {
+            Console.Write(prompt);
+            int input = Convert.ToInt32(Console.ReadLine());
+            return input;
+        }
+        static void OddOrEven()
+        {
+            do
+            {
+                if (OddOrEvenCheck())
+                {
+                    Console.WriteLine($"\nEsse número é par!");
+                    number = GetNumber("\nPor favor, digite um número ímpar: ");
+                }
+                else
+                    break;
+            } while (true);
+        }
+        static bool OddOrEvenCheck()
+        {
+            return number % 2 == 0;
         }
     }
 }
