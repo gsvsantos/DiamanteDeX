44840b120a3383d005d4bd1d6646eb2a4c14e977
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-25 16:59:35 -0300

    Add project files.

---
+++ DiamanteDeX/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DiamanteDeX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
