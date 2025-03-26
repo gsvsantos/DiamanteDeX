8d3d9e22b00fa445a5a57b434c00d43850e8a2b1
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-26 02:38:35 -0300

    refactor: Adicionado método `Header()`.

---
+++ DiamanteDeX/Entities/Utils/ViewWrite.cs
@@ -0,0 +1,13 @@
+﻿namespace DiamanteDeX.Entities.Utils
+{
+    public class ViewWrite
+    {
+        public static void Header()
+        {
+            Console.Clear();
+            ViewColors.PaintWriteLine("/=-=-=-=-=-=-=-=-=-=-=-=\\");
+            ViewColors.PaintWriteLine("      Diamante de X", ConsoleColor.Cyan);
+            ViewColors.PaintWriteLine("\\=-=-=-=-=-=-=-=-=-=-=-=/");
+        }
+    }
+}
