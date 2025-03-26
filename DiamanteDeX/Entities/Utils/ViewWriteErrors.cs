1523bb713a77f2ed5989bd62c1c3f8363d616c13
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-26 05:22:28 -0300

    feat: Adicionado métodos que lidam com mensagens de erros.

--- DiamanteDeX/Entities/Utils/ViewWriteErrors.cs
+++ DiamanteDeX/Entities/Utils/ViewWriteErrors.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DiamanteDeX.Entities.Utils
+﻿namespace DiamanteDeX.Entities.Utils
 {
-    internal class ViewWriteErrors
+    public class ViewWriteErrors
     {
+        public static void IsNotANumber(string input)
+        {
+            ViewColors.PaintWriteLine($"'{input}' não é um número...", ConsoleColor.Red);
+        }
+        public static void NumberWithChar()
+        {
+            ViewColors.PaintWriteLine("O valor não pode conter espaço ' ', ponto (.), ou vírgula (,)!", ConsoleColor.Red);
+        }
+        public static void InvalidNumber()
+        {
+            ViewColors.PaintWriteLine("O valor digitado não é um número válido.", ConsoleColor.Red);
+        }
+        public static void NumberBetweenMinAndMax(int maxValue)
+        {
+            ViewColors.PaintWriteLine($"O valor deve estar entre 3 e {maxValue}.", ConsoleColor.Red);
+        }
+        public static void IsNotAEven()
+        {
+            ViewColors.PaintWriteLine($"\nEsse número é inválido! Tente novamente.", ConsoleColor.Red);
+        }
     }
 }
