b3a3da622416c46fa6add13ac2c9a52c51f61a73
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-26 05:21:02 -0300

    refactor: Movido métodos de mensagens de erros para `ViewWriteErrors`.

--- DiamanteDeX/Entities/Utils/ViewWrite.cs
+++ DiamanteDeX/Entities/Utils/ViewWrite.cs
@@ -13,25 +13,5 @@
         {
             ViewColors.PaintWriteLine($"\nO número escolhido é: {Diamond.QuantityOfX}\n", ConsoleColor.Green);
         }
-        public static void IsNotANumber(string input)
-        {
-            ViewColors.PaintWriteLine($"'{input}' não é um número...", ConsoleColor.Red);
-        }
-        public static void NumberWithChar()
-        {
-            ViewColors.PaintWriteLine("O valor não pode conter espaço ' ', ponto (.), ou vírgula (,)!", ConsoleColor.Red);
-        }
-        public static void InvalidNumber()
-        {
-            ViewColors.PaintWriteLine("O valor digitado não é um número válido.", ConsoleColor.Red);
-        }
-        public static void NumberBetweenMinAndMax(int maxValue)
-        {
-            ViewColors.PaintWriteLine($"O valor deve estar entre 3 e {maxValue}.", ConsoleColor.Red);
-        }
-        public static void IsNotAEven()
-        {
-            ViewColors.PaintWriteLine($"\nEsse número é inválido! Tente novamente.", ConsoleColor.Red);
-        }
     }
 }
