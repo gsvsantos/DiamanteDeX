9c9f41755019130b045b163774ffa07593005f84
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-26 02:14:54 -0300

    refactor: `PaintWriteLine()` e `PaintWrite()` movidos para `ViewColors`.

--- DiamanteDeX/Entities/Utils/ViewUtils.cs
+++ DiamanteDeX/Entities/Utils/ViewUtils.cs
@@ -1,32 +1,21 @@
 ﻿namespace DiamanteDeX.Entities.Utils
+
 {
     public class ViewUtils
     {
-        public static void PaintWriteLine(string message, ConsoleColor color = ConsoleColor.White)
-        {
-            Console.ForegroundColor = color;
-            Console.WriteLine(message);
-            Console.ResetColor();
-        }
-        public static void PaintWrite(string message, ConsoleColor color = ConsoleColor.White)
-        {
-            Console.ForegroundColor = color;
-            Console.Write(message);
-            Console.ResetColor();
-        }
         public static void Header()
         {
             Console.Clear();
-            PaintWriteLine("/=-=-=-=-=-=-=-=-=-=-=-=\\");
-            PaintWriteLine("      Diamante de X", ConsoleColor.Cyan);
-            PaintWriteLine("\\=-=-=-=-=-=-=-=-=-=-=-=/");
+            ViewColors.PaintWriteLine("/=-=-=-=-=-=-=-=-=-=-=-=\\");
+            ViewColors.PaintWriteLine("      Diamante de X", ConsoleColor.Cyan);
+            ViewColors.PaintWriteLine("\\=-=-=-=-=-=-=-=-=-=-=-=/");
         }
         public static void PressEnter(string type)
         {
             switch (type)
             {
                 case "USAR-NOVAMENTE":
-                    PaintWrite("\nPressione [Enter] para usar novamente.", ConsoleColor.Yellow);
+                    ViewColors.PaintWrite("\nPressione [Enter] para usar novamente.", ConsoleColor.Yellow);
                     Console.ReadKey();
                     break;
             }
